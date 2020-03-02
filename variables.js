0ecf2f57622fc45006904bcc6248076cd4ca32f3
githumbi <githumbi74@gmail.com>
2020-03-02 16:41:11 +0300

    const

---
+++ variables.js
@@ -0,0 +1,6 @@
+function ConstFuntion() {
+    var pie = 12;
+    pie = 14;
+    console.log(pie);
+}
+ConstFuntion();
---
+++ variables.ts
@@ -0,0 +1,8 @@
+//declare var variables
+function ConstFuntion(){
+    const pie = 12
+    pie = 14
+console.log(pie)
+}
+
+ConstFuntion()
